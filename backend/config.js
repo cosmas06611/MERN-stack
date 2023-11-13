e885382f34124fa5730254458833f8b35cb40d3d
khosi <cosmasndulue00611@gmail.com>
2023-11-13 14:25:48 +0100

    adding all repository

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 4190;
+
+export const mongoDBURL = 
+'mongodb+srv://root:root@book.1h4q29d.mongodb.net/books-collection?retryWrites=true&w=majority'
---
+++ backend/index.js
@@ -0,0 +1,49 @@
+import express from "express"
+import { PORT, mongoDBURL } from "./config.js"
+import mongoose from 'mongoose'
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors'
+
+const app = express(); 
+
+app.use(express.json());
+
+// middleware for handling CORS POLICY
+// Option1: allow all origins with default of cors(*)
+
+app.use(cors());
+
+
+// option 2: allow custom origins
+// app.use(
+//     cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+// })
+// )
+
+
+
+app.get('/', (request, response) =>{
+    console.log(request)
+    return response.status(234).send('welcome to MERN STACH TUTORIAL');
+});
+
+app.use('/books', booksRoute);
+
+
+mongoose
+.connect(mongoDBURL)
+.then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+        console.log(`App is listening to port: ${PORT}`)
+        
+    });
+})
+.catch((error) => {
+console.log(error);
+});
+
+
